1cb274ad26e0b7f609fbe7a2796c9554e350cdc6
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-10-07 21:22:23 +0100

    added header

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Canvas } from "@react-three/fiber"
 export default function App() {
   return (
     <div>
+      <h1>Game of Life</h1>
       <Canvas orthographic camera={{ left: -1, right: 1, bottom: -1, top: 1 }} style={{ background: "black" }}>
         <GameOfLife width={200} height={200} />
       </Canvas>
