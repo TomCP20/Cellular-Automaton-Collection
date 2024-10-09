3e37352eb181f5ca7c7aee5f778a6c01774ea0a7
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-10-09 23:42:51 +0100

    refactored css

--- src/components/NavBarElement.tsx
+++ src/components/NavBarElement.tsx
@@ -2,11 +2,9 @@ import { ReactNode } from "react";
 import { NavLink } from "react-router-dom";
 
 export function NavBarElement({ children, to }: Readonly<{ children: ReactNode; to: string; }>) {
-  const activeLink = "block p-[16px] bg-slate-800";
-  const normalLink = "block p-[16px]";
   return (
-    <li className="float-left text-white text-center hover:bg-slate-800">
-      <NavLink to={to} className={({ isActive }) => (isActive ? activeLink : normalLink)}>
+    <li className="float-left">
+      <NavLink to={to} className={({ isActive }) => ["block p-[16px] text-white text-center hover:bg-slate-800", (isActive ? "bg-slate-800" : "")].join(" ")}>
         {children}
       </NavLink>
     </li>
