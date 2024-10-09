6b5075940ee3b429e93ab915f108449ec0e44704
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-10-09 23:45:01 +0100

    made canvas background black

--- src/pages/GameOfLife.tsx
+++ src/pages/GameOfLife.tsx
@@ -8,17 +8,16 @@ export function GameOfLife() {
   const world = useRef(new World(200));
   const play = useRef(false);
   const step = useRef(false);
-  
   return (
     <div className="h-auto flex p-0">
       <div className="flex-1 self-center text-center" />
       <div className="size-[800px] text-center self-center">
-        <Canvas orthographic camera={{ left: -1, right: 1, bottom: -1, top: 1 }}>
+        <Canvas orthographic camera={{ left: -1, right: 1, bottom: -1, top: 1 }} className="bg-black">
           <GameOfLifeMesh world={world} play={play} step={step} />
         </Canvas>
       </div>
       <div className="flex-1 self-start text-left verti">
-        <GameOfLifeUI play={play} world={world} step={step}/>
+        <GameOfLifeUI play={play} world={world} step={step} />
       </div>
     </div>
   );
--- src/pages/WolframCode.tsx
+++ src/pages/WolframCode.tsx
@@ -6,21 +6,21 @@ import { RuleDropdown } from "../components/RuleDropdown";
 import { Button } from "../components/Button";
 
 export function WolframCode() {
-    const [rule, setRule] = useState(30);
-    const [size, setSize] = useState(800);
-    return (
-      <div className="h-auto flex p-0">
-        <div className="flex-1 self-center text-center" />
-        <div className="size-[800px] text-center self-center">
-          <Canvas orthographic camera={{ left: -1, right: 1, bottom: -1, top: 1 }}>
-            <WolframCodeMesh size={size} ruleNo={rule}/>
-          </Canvas>
-        </div>
-        <div className="flex-1 self-start text-left verti">
-          <RuleDropdown rule={rule} setRule={setRule}/><br/>
-          <Button onClick={() => setRule(Math.floor(Math.random() * 256))}>Random</Button><br/>
-          <SizeDropdown size={size} setSize={setSize}/>
-        </div>
+  const [rule, setRule] = useState(30);
+  const [size, setSize] = useState(800);
+  return (
+    <div className="h-auto flex p-0">
+      <div className="flex-1 self-center text-center" />
+      <div className="size-[800px] text-center self-center">
+        <Canvas orthographic camera={{ left: -1, right: 1, bottom: -1, top: 1 }} className="bg-black">
+          <WolframCodeMesh size={size} ruleNo={rule} />
+        </Canvas>
       </div>
-    );
-  }
+      <div className="flex-1 self-start text-left verti">
+        <RuleDropdown rule={rule} setRule={setRule} /><br />
+        <Button onClick={() => setRule(Math.floor(Math.random() * 256))}>Random</Button><br />
+        <SizeDropdown size={size} setSize={setSize} />
+      </div>
+    </div>
+  );
+}
