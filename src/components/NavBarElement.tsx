123b8603eae1fd7ab5f76f5fe3ab817fc6c3ee68
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-10-09 23:39:07 +0100

    added navbar highlighting

--- src/components/NavBarElement.tsx
+++ src/components/NavBarElement.tsx
@@ -1,6 +1,14 @@
 import { ReactNode } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export function NavBarElement({ children, to }: Readonly<{ children: ReactNode; to: string; }>) {
-  return <li className="float-left"><Link to={to} className="block text-white text-center p-[16px] text-dec hover:bg-slate-800">{children}</Link></li>;
+  const activeLink = "block p-[16px] bg-slate-800";
+  const normalLink = "block p-[16px]";
+  return (
+    <li className="float-left text-white text-center hover:bg-slate-800">
+      <NavLink to={to} className={({ isActive }) => (isActive ? activeLink : normalLink)}>
+        {children}
+      </NavLink>
+    </li>
+  );
 }
