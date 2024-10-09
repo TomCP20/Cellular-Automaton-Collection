79288629bf5275a4e3b20054298c2c461a6b673a
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-10-09 21:23:31 +0100

    step button is disabled when the world is in play mode

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,8 @@
 import { ReactNode } from "react";
 
-export function Button({ children, onClick }: Readonly<{ children: ReactNode; onClick?: () => void; }>) {
+export function Button({ children, onClick, disabled }: Readonly<{ children: ReactNode; onClick?: () => void; disabled?: boolean }>) {
 
   return (
-    <button onClick={onClick} className="text-center m-0.5 p-1 w-20 rounded-sm border border-solid border-black bg-gray-200 hover:bg-gray-300">{children}</button>
+    <button onClick={onClick} disabled={disabled} className="text-center m-0.5 p-1 w-20 rounded-sm border border-solid border-black bg-gray-200 hover:bg-gray-300 disabled:bg-gray-400">{children}</button>
   );
 }
--- src/pages/GameOfLife.tsx
+++ src/pages/GameOfLife.tsx
@@ -1,19 +1,22 @@
 import { Canvas } from "@react-three/fiber";
 import { useState, useRef, useEffect } from "react";
 import { Button } from "../components/Button";
-import { PausePlayButton } from "../components/PausePlayButton";
 import { SizeDropdown } from "../components/SizeDropdown";
 import { GameOfLifeMesh } from "../meshes/GameOfLifeMesh";
 import World from "../World";
 
 export function GameOfLife() {
   const [size, setSize] = useState(200);
+  const [playState, setPlayState] = useState(false);
   const world = useRef(new World(size, size));
   const play = useRef(false);
   const step = useRef(false);
   useEffect(() => {
     world.current = new World(size, size);
   }, [size]);
+  useEffect(() => {
+    play.current = playState;
+  }, [playState]);
   return (
     <div className="h-auto flex p-0">
       <div className="flex-1 self-center text-center" />
@@ -23,10 +26,10 @@ export function GameOfLife() {
         </Canvas>
       </div>
       <div className="flex-1 self-start text-left verti">
-        <PausePlayButton play={play} /><br />
+        <Button onClick={() => setPlayState(!playState)}>{playState ? "pause" : "play"}</Button><br />
         <Button onClick={() => world.current.Noise()}>noise</Button><br />
         <Button onClick={() => world.current.Clear()}>clear</Button><br />
-        <Button onClick={() => step.current = true}>step</Button><br />
+        <Button onClick={() => step.current = true} disabled={playState}>step</Button><br />
         <SizeDropdown size={size} setSize={setSize} />
       </div>
     </div>
