e460147c2d57f7c71917495aea5d45fee766996c
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-10-10 20:20:22 +0100

    added home page

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import { HashRouter, Route, Routes } from "react-router-dom";
 import { NavBar } from "./components/NavBar";
 import { WolframCode } from "./pages/WolframCode";
 import { ThreeDGameOfLife } from "./pages/ThreeDGameOfLife";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
     <HashRouter>
       <NavBar />
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Home />} />
         <Route path="/1d" element={<WolframCode />} />
         <Route path="/2d" element={<GameOfLife />} />
         <Route path="/3d" element={<ThreeDGameOfLife />} />
---
+++ src/pages/Home.tsx
@@ -0,0 +1,53 @@
+import wolfram from "../images/Wolfram.png";
+import gameoflife from "../images/GameOfLife.png";
+import threedgameoflife from "../images/3DGameOfLife.png"
+import { ReactNode } from "react";
+
+export default function Home() {
+    return (
+        <>
+            <h1 className="text-center text-2xl m-5">Cellular Automaton Collection.</h1>
+            <div className="grid grid-cols-2 gap-4 auto-cols-min w-[50%] m-auto p-4">
+                <CaptionedImage imageSrc={wolfram} title="Wolfram Code" link="https://en.wikipedia.org/wiki/Wolfram_code">
+                    The Wolfram Code is a numbering system for 1D Celluar Automata created by Stephen Wolfram in 1983.
+                </CaptionedImage>
+                <CaptionedImage imageSrc={gameoflife} title="Game Of Life" link="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life">
+                    The Game of Life, or Conway's Game of Life, is a 2D cellular automata created by John Horton Conway in 1970. It is arguably the best-known cellular automata.
+                    <br />
+                    The rules are:
+                    <ul className="list-disc list-inside">
+                        <li>A cell is either alive or dead.</li>
+                        <li>A dead cell becomes alive if it has exactly 3 neighbours.</li>
+                        <li>An alive cell stays alive if it has 2 or 3 neighbours.</li>
+                        <li>Otherwise, the cell dies/remains dead.</li>
+                    </ul>
+                    These simple rules give rise to complex behaviour such as turning completeness.
+                </CaptionedImage>
+                <CaptionedImage imageSrc={threedgameoflife} title="3D Game Of Life" link="https://conwaylife.com/wiki/Three-dimensional_cellular_automaton">
+                    The 3D Game of Life is a 3D Celluar Automata 3D Game Of Life by Carter Bays in 1987 that attempts to project the rules of The Game of Life to 3 Dimensions.
+                    <br />
+                    The Celluar Automata has the following rules.
+                    <ul className="list-disc list-inside">
+                        <li>A cell is either alive or dead.</li>
+                        <li>A dead cell becomes alive if it has exactly 6 neighbours.</li>
+                        <li>An alive cell stays alive with 5, 6 or 7 neighbours.</li>
+                        <li>Otherwise, the cell dies/remains dead.</li>
+                    </ul>
+                    He found that these rules must closely recreate the behaviour of Life.
+                </CaptionedImage>
+            </div>
+        </>
+    );
+}
+
+function CaptionedImage({ imageSrc, title, link, children }: Readonly<{ imageSrc: string; title: string; link: string; children: ReactNode; }>) {
+    return <>
+        <div className=" p-1">
+            <h2 className="text-center text-xl">{title}</h2>
+            {children}
+            <br />
+            <a href={link} className="underline text-blue-600 visited:text-purple-600">Read More</a>
+        </div>
+        <img src={imageSrc} alt={title} className="size-[400px] bg-black" />
+    </>;
+}
