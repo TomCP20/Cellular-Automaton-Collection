89d618f9504a7e9da19adabbf8586c34de9e7ad9
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-10-10 15:21:10 +0100

    basic update logic

--- src/meshes/Cubes.tsx
+++ src/meshes/Cubes.tsx
@@ -1,60 +1,44 @@
 import { Instance, Instances } from "@react-three/drei";
-import { useRef, useEffect } from "react";
-import { Object3D, InstancedMesh } from "three";
+import { useFrame } from "@react-three/fiber";
+import { Color } from "three";
 
-export function Cubes() {
-  const size = 5;
-  const temp = new Object3D();
-  const instancedMeshRef = useRef<InstancedMesh>(null!);
-  const world: boolean[][][] = [];
-  for (let x = 0; x < size; x++) {
-    world[x] = []
-    for (let y = 0; y < size; y++) {
-      world[x][y] = []
-      for (let z = 0; z < size; z++) {
-        world[x][y][z] = Math.random() >= 0.5;
-      }
-    }
-  }
-  useEffect(() => {
-    // Set positions
-    if (instancedMeshRef.current) {
-      for (let x = 0; x < size; x++) {
-        for (let y = 0; y < size; y++) {
-          for (let z = 0; z < size; z++) {
-            const i = x * size * size + y * size + z;
-            temp.position.set((x - (size - 1) / 2) / size, (y - (size - 1) / 2) / size, (z - (size - 1) / 2) / size);
-            temp.updateMatrix();
-            instancedMeshRef.current.setMatrixAt(i, temp.matrix);
-          }
-        }
-      }
-
-      // Update the instance
-      instancedMeshRef.current.instanceMatrix.needsUpdate = true;
-    }
-  });
+export function Cubes({ size, world, setWorld }: Readonly<{ size: number; world: boolean[][][]; setWorld: React.Dispatch<React.SetStateAction<boolean[][][]>>; }>) {
+  useFrame(() => {
+    setWorld(stepWorld(size, world));
+  })
   const index = Array.from(Array(size), (_e, i) => { return i });
   return (
     <Instances limit={size * size * size}>
       <boxGeometry />
-      <meshStandardMaterial />
+      <meshBasicMaterial />
+      
       {index.map((x) => {
         return <>
           {index.map((y) => {
             return <>
               {index.map((z) => {
-                return world[x][y][z] ? <Box key={x * size * size + y * size + z} size={size} position={[x, y, z]} /> : null
+                return world[x][y][z] ? <Instance key={`${x}${y}${z}`} color={new Color(x/size, y/size, z/size)} scale={1 / size} position={[getPos(x, size), getPos(y, size), getPos(z, size)]} /> : null
               })}</>
           })}</>
       })}
     </Instances>
   );
 }
-function Box({ size, position }: Readonly<{ size: number; position: [number, number, number]; }>) {
-  const [x, y, z] = position;
-  return (
-    <Instance color="red" scale={1 / size} position={[(x - (size - 1) / 2) / size, (y - (size - 1) / 2) / size, (z - (size - 1) / 2) / size]} />
-  );
+
+function getPos(n: number, size: number): number {
+  return (n - (size - 1) / 2) / size;
 }
 
+function stepWorld(size: number, world: boolean[][][]): boolean[][][] {
+  const newworld: boolean[][][] = Array(size);
+  for (let x = 0; x < size; x++) {
+    newworld[x] = Array(size);
+    for (let y = 0; y < size; y++) {
+      newworld[x][y] = Array(size);
+      for (let z = 0; z < size; z++) {
+        newworld[x][y][z] = !world[x][y][z];
+      }
+    }
+  }
+  return newworld;
+}
--- src/pages/ThreeDGameOfLife.tsx
+++ src/pages/ThreeDGameOfLife.tsx
@@ -1,21 +1,41 @@
-import { OrbitControls } from "@react-three/drei";
+import { Edges, OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Cubes } from "../meshes/Cubes";
+import { useState } from "react";
 
 export function ThreeDGameOfLife() {
+  const size = 10;
+  const [world, setWorld] = useState(GenWorld(size))
   return (
     <div className="h-auto flex p-0">
       <div className="flex-1 self-center text-center" />
       <div className="size-[800px] text-center self-center">
         <Canvas className="bg-black">
-          <ambientLight intensity={Math.PI / 2} />
-          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-          <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-          <Cubes />
+          <PerspectiveCamera makeDefault position={[-2, -2, -2]} frames={100} />
+          <Cubes size={size} world={world} setWorld={setWorld} />
+          <mesh>
+            <boxGeometry />
+            <meshBasicMaterial transparent={true} opacity={0} />
+            <Edges lineWidth={2} scale={1} threshold={15} color="white" />
+          </mesh>
           <OrbitControls />
         </Canvas>
       </div>
       <div className="flex-1 self-start text-left verti" />
     </div>
   );
+}
+
+function GenWorld(size: number) {
+  const world: boolean[][][] = Array(size);
+  for (let x = 0; x < size; x++) {
+    world[x] = Array(size);
+    for (let y = 0; y < size; y++) {
+      world[x][y] = Array(size);
+      for (let z = 0; z < size; z++) {
+        world[x][y][z] = Math.random() >= 0.5;
+      }
+    }
+  }
+  return world;
 }
