dc37305f0ff1045fbaa4020c746d2deb502df164
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-10-10 02:59:12 +0100

    progress on instancing

--- src/meshes/Cubes.tsx
+++ src/meshes/Cubes.tsx
@@ -1,10 +1,20 @@
 import { useRef, useEffect } from "react";
-import { Object3D, InstancedMesh, Color } from "three";
+import { Object3D, InstancedMesh } from "three";
 
 export function Cubes() {
   const size = 10;
   const temp = new Object3D();
   const instancedMeshRef = useRef<InstancedMesh>(null!);
+  const world: boolean[][][] = [];
+  for (let x = 0; x < size; x++) {
+    world[x] = []
+    for (let y = 0; y < size; y++) {
+      world[x][y] = []
+      for (let z = 0; z < size; z++) {
+          world[x][y][z] = Math.random() >= 0.5;
+      }
+    }
+  }
   useEffect(() => {
     // Set positions
     if (instancedMeshRef.current) {
@@ -12,16 +22,9 @@ export function Cubes() {
         for (let y = 0; y < size; y++) {
           for (let z = 0; z < size; z++) {
             const i = x * size * size + y * size + z;
-            temp.position.set(x - (size - 1) / 2, y - (size - 1) / 2, z - (size - 1) / 2);
+            temp.position.set((x - (size - 1) / 2)/size, (y - (size - 1) / 2)/size, (z - (size - 1) / 2)/size);
             temp.updateMatrix();
             instancedMeshRef.current.setMatrixAt(i, temp.matrix);
-            if ((x + y + z) % 2 === 0) {
-              instancedMeshRef.current.setColorAt(i, new Color("red"));
-            }
-            else {
-              instancedMeshRef.current.setColorAt(i, new Color("green"));
-            }
-
           }
         }
       }
@@ -32,8 +35,8 @@ export function Cubes() {
   });
   return (
     <instancedMesh ref={instancedMeshRef} args={[null, null, size * size * size]}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshBasicMaterial />
+      <boxGeometry args={[1/size, 1/size, 1/size]} />
+      <meshBasicMaterial color={"red"} />
     </instancedMesh>
   );
 }
