a098d8f7aeea121aab4ced252f0b7b4193f9841b
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-10-12 22:09:44 +0100

    refactored  instance updating

--- src/meshes/ThreeDGameOfLifeMesh.tsx
+++ src/meshes/ThreeDGameOfLifeMesh.tsx
@@ -1,5 +1,5 @@
 import { useFrame } from "@react-three/fiber";
-import { useEffect, useMemo, useRef } from "react";
+import { MutableRefObject, useEffect, useMemo, useRef } from "react";
 import { Color, InstancedMesh, Object3D } from "three";
 
 export function ThreeDGameOfLifeMesh({ size, world }: Readonly<{ size: number; world: boolean[][][]; }>) {
@@ -9,39 +9,11 @@ export function ThreeDGameOfLifeMesh({ size, world }: Readonly<{ size: number; w
   useEffect(() => {
     if (meshRef == null) return;
     if (meshRef.current == null) return;
-
-    let i = 0;
-    for (let x = 0; x < size; x++) {
-      for (let y = 0; y < size; y++) {
-        for (let z = 0; z < size; z++) {
-          const id = i++;
-          tempObject.position.set(getPos(x, size), getPos(y, size), getPos(z, size));
-          tempObject.scale.set(1/size, 1/size, 1/size);
-          tempObject.updateMatrix();
-          meshRef.current.setMatrixAt(id, tempObject.matrix);
-          meshRef.current.setColorAt(id, new Color(x/size, y/size, z/size));
-        }
-      }
-    }
-    meshRef.current.instanceMatrix.needsUpdate = true;
-  }, [size, tempObject]);
+    UpdateMesh(size, tempObject, world, meshRef);
+  }, [size, tempObject, world]);
 
   useFrame(() => {
-    let i = 0;
-    for (let x = 0; x < size; x++) {
-      for (let y = 0; y < size; y++) {
-        for (let z = 0; z < size; z++) {
-          const id = i++;
-          tempObject.position.set(getPos(x, size), getPos(y, size), getPos(z, size));
-          const scale = world[x][y][z] ? 1/size : 0;
-          tempObject.scale.set(scale, scale, scale);
-          tempObject.updateMatrix();
-          meshRef.current.setMatrixAt(id, tempObject.matrix);
-          meshRef.current.setColorAt(id, new Color(x/size, y/size, z/size));
-        }
-      }
-    }
-    meshRef.current.instanceMatrix.needsUpdate = true;
+    UpdateMesh(size, tempObject, world, meshRef);
   })
   return (
     <instancedMesh ref={meshRef} args={[undefined, undefined, size*size*size]}>
@@ -49,6 +21,24 @@ export function ThreeDGameOfLifeMesh({ size, world }: Readonly<{ size: number; w
       <meshBasicMaterial />
     </instancedMesh>
   );
+}
+
+function UpdateMesh(size: number, tempObject: Object3D, world: boolean[][][], meshRef: MutableRefObject<InstancedMesh>) {
+  let i = 0;
+  for (let x = 0; x < size; x++) {
+    for (let y = 0; y < size; y++) {
+      for (let z = 0; z < size; z++) {
+        const id = i++;
+        tempObject.position.set(getPos(x, size), getPos(y, size), getPos(z, size));
+        const scale = world[x][y][z] ? 1 / size : 0;
+        tempObject.scale.set(scale, scale, scale);
+        tempObject.updateMatrix();
+        meshRef.current.setMatrixAt(id, tempObject.matrix);
+        meshRef.current.setColorAt(id, new Color(x / size, y / size, z / size));
+      }
+    }
+  }
+  meshRef.current.instanceMatrix.needsUpdate = true;
 }
 
 function getPos(n: number, size: number): number {
