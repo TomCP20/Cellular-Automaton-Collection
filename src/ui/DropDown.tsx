0cfd6abe22d7e19054715ab29fad69ce204ceedd
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-10-12 23:05:46 +0100

    can't select the text of dropdown label

--- src/ui/DropDown.tsx
+++ src/ui/DropDown.tsx
@@ -1,6 +1,6 @@
 export function DropDown({ val, setVal, vals, label, optionLabel }: Readonly<{ val: number; setVal: React.Dispatch<React.SetStateAction<number>>; vals: number[]; label: string; optionLabel: (s: number) => string; }>) {
     return (
-        <label className="m-1">
+        <label className="m-1 select-none">
             {label}<br />
             <select value={val} onChange={e => setVal(+e.target.value)} className="m-1 min-w-20">
                 {vals.map((s) => <option key={s} value={s}>{optionLabel(s)}</option>)}
